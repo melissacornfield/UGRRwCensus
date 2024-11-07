91ea306cce545c5494291c206a6f67403f7025db
Melissa <melissacornfield@gmail.com>
2024-11-07 17:38:31 -0600

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -24,7 +24,7 @@ var lyr_1860IllinoisCounties_AggregateAdded_1 = new ol.layer.Vector({
                 source:jsonSource_1860IllinoisCounties_AggregateAdded_1, 
                 style: style_1860IllinoisCounties_AggregateAdded_1,
                 popuplayertitle: "1860IllinoisCounties_AggregateAdded",
-                interactive: true,
+                interactive: false,
     title: '1860IllinoisCounties_AggregateAdded<br />\
     <img src="styles/legend/1860IllinoisCounties_AggregateAdded_1_0.png" /> 1979 - 9237<br />\
     <img src="styles/legend/1860IllinoisCounties_AggregateAdded_1_1.png" /> 9237 - 11243<br />\
@@ -44,7 +44,7 @@ var lyr_1840IllinoisCounties_AggregateAdded_2 = new ol.layer.Vector({
                 source:jsonSource_1840IllinoisCounties_AggregateAdded_2, 
                 style: style_1840IllinoisCounties_AggregateAdded_2,
                 popuplayertitle: "1840IllinoisCounties_AggregateAdded",
-                interactive: true,
+                interactive: false,
     title: '1840IllinoisCounties_AggregateAdded<br />\
     <img src="styles/legend/1840IllinoisCounties_AggregateAdded_2_0.png" /> 1979 - 9237<br />\
     <img src="styles/legend/1840IllinoisCounties_AggregateAdded_2_1.png" /> 9237 - 11243<br />\
@@ -78,7 +78,7 @@ lyr_1840IllinoisCounties_AggregateAdded_2.set('fieldImages', {'NAME': 'TextEdit'
 lyr_UGRRMappingProjectDataSetNov42024_3.set('fieldImages', {'Title': 'TextEdit', 'Name(s) related to site': 'TextEdit', 'freedom seeker name related to site': 'TextEdit', 'short description': 'TextEdit', 'More information': 'TextEdit', 'type (stationary)': 'TextEdit', 'Routes': 'TextEdit', 'Town/ City': 'TextEdit', 'present day county': 'TextEdit', 'state': 'TextEdit', 'date of settlement in Illinois': 'TextEdit', 'Time Active': 'TextEdit', 'Date Range of Travel Through Illinois': 'TextEdit', 'source type': 'TextEdit', 'Reliability': 'TextEdit', 'lat': 'TextEdit', 'lon': 'TextEdit', 'geoAddress': 'TextEdit', 'Nearest Current Address': 'TextEdit', 'Access': 'TextEdit', 'Start Year': 'TextEdit', 'End Year': 'TextEdit', 'entered by': 'TextEdit', 'entry date': 'TextEdit', 'link to data entry template': 'TextEdit', });
 lyr_1860IllinoisCounties_AggregateAdded_1.set('fieldLabels', {'NAME': 'no label', 'ID': 'no label', 'STATE': 'no label', 'FIPS': 'no label', 'VERSION': 'no label', 'START_DATE': 'no label', 'END_DATE': 'no label', 'CHANGE': 'no label', 'CITATION': 'no label', 'START_N': 'no label', 'END_N': 'no label', 'AREA_SQMI': 'no label', 'DATASET': 'no label', 'CNTY_TYPE': 'no label', 'FULL_NAME': 'no label', '1860 popul': 'no label', '1860 pop_1': 'no label', });
 lyr_1840IllinoisCounties_AggregateAdded_2.set('fieldLabels', {'NAME': 'no label', 'ID': 'no label', 'STATE': 'no label', 'FIPS': 'no label', 'VERSION': 'no label', 'START_DATE': 'no label', 'END_DATE': 'no label', 'CHANGE': 'no label', 'CITATION': 'no label', 'START_N': 'no label', 'END_N': 'no label', 'AREA_SQMI': 'no label', 'DATASET': 'no label', 'CNTY_TYPE': 'no label', 'FULL_NAME': 'no label', '1840 revis': 'no label', '1840 rev_1': 'no label', });
-lyr_UGRRMappingProjectDataSetNov42024_3.set('fieldLabels', {'Title': 'inline label - visible with data', 'Name(s) related to site': 'no label', 'freedom seeker name related to site': 'no label', 'short description': 'inline label - visible with data', 'More information': 'no label', 'type (stationary)': 'inline label - visible with data', 'Routes': 'no label', 'Town/ City': 'inline label - visible with data', 'present day county': 'no label', 'state': 'no label', 'date of settlement in Illinois': 'no label', 'Time Active': 'no label', 'Date Range of Travel Through Illinois': 'no label', 'source type': 'no label', 'Reliability': 'no label', 'lat': 'no label', 'lon': 'no label', 'geoAddress': 'no label', 'Nearest Current Address': 'no label', 'Access': 'no label', 'Start Year': 'no label', 'End Year': 'no label', 'entered by': 'no label', 'entry date': 'no label', 'link to data entry template': 'no label', });
+lyr_UGRRMappingProjectDataSetNov42024_3.set('fieldLabels', {'Title': 'inline label - visible with data', 'Name(s) related to site': 'hidden field', 'freedom seeker name related to site': 'hidden field', 'short description': 'inline label - visible with data', 'More information': 'hidden field', 'type (stationary)': 'inline label - visible with data', 'Routes': 'hidden field', 'Town/ City': 'inline label - visible with data', 'present day county': 'hidden field', 'state': 'hidden field', 'date of settlement in Illinois': 'hidden field', 'Time Active': 'hidden field', 'Date Range of Travel Through Illinois': 'hidden field', 'source type': 'hidden field', 'Reliability': 'hidden field', 'lat': 'hidden field', 'lon': 'hidden field', 'geoAddress': 'hidden field', 'Nearest Current Address': 'hidden field', 'Access': 'hidden field', 'Start Year': 'hidden field', 'End Year': 'hidden field', 'entered by': 'hidden field', 'entry date': 'hidden field', 'link to data entry template': 'hidden field', });
 lyr_UGRRMappingProjectDataSetNov42024_3.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
